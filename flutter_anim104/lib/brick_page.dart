2c942684b5efdfed4ff195b8d7912bc118898ba5
Dreamwalker <aristojeff@gmail.com>
2018-12-14 14:35:12 +0900

    animation added

--- flutter_anim104/lib/brick_page.dart
+++ flutter_anim104/lib/brick_page.dart
@@ -5,12 +5,63 @@ class BrickPage extends StatefulWidget {
   _BrickPageState createState() => _BrickPageState();
 }
 
-class _BrickPageState extends State<BrickPage> {
+class _BrickPageState extends State<BrickPage> with TickerProviderStateMixin {
+  AnimationController animationController;
+  Tween tween;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 5));
+    tween = Tween(begin: 0.0, end: 1.0);
+  }
+  //Bricks one
+  Animation get animOne => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.0, 0.125, curve: Curves.linear)));
+
+  Animation get animTwo => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.125, 0.25, curve: Curves.linear)));
+
+  //TODO brick Two
+  Animation get animThree => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.25, 0.375, curve: Curves.linear)));
+
+  //TODO brick Three
+  Animation get animFour => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.375, 0.5, curve: Curves.linear)));
+
+  //TODO brick Four
+  Animation get animFive => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.5, 0.625, curve: Curves.linear)));
+
+  //TODO brick Four
+  Animation get animSix => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.625, 0.75, curve: Curves.linear)));
+
+  //TODO brick Seven
+  Animation get animSeven => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.75, 0.875, curve: Curves.linear)));
+
+  //TODO brick Seven
+  Animation get animEight => tween.animate(CurvedAnimation(
+      parent: animationController,
+      curve: Interval(0.875, 1.0, curve: Curves.linear)));
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Animation 104"),
+        title: Text("Flutter Animation 104"),
       ),
       body: Center(
         child: Row(
@@ -28,7 +79,6 @@ class _BrickPageState extends State<BrickPage> {
 }
 
 class Brick extends StatelessWidget {
-
   final double marginLeft;
 
   Brick({this.marginLeft = 15.0});
@@ -42,8 +92,7 @@ class Brick extends StatelessWidget {
       height: 10.0,
 //      color: Colors.green,
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15.0),
-          color: Colors.green),
+          borderRadius: BorderRadius.circular(15.0), color: Colors.green),
     );
   }
 }
