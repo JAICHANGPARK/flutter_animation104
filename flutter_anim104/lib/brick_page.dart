fc80596cc88ccb6fddab54319047f2fb9e7e18ad
Dreamwalker <aristojeff@gmail.com>
2018-12-14 14:23:09 +0900

    fix code

--- flutter_anim104/lib/brick_page.dart
+++ flutter_anim104/lib/brick_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class BrickPage extends StatefulWidget {
   @override
   _BrickPageState createState() => _BrickPageState();
@@ -13,13 +12,29 @@ class _BrickPageState extends State<BrickPage> {
       appBar: AppBar(
         title: Text("Animation 104"),
       ),
-      body: Center(
-        child: Container(
-          width: 40.0,
-          height: 10.0,
-          color: Colors.green,
-        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Brick(),
+          Brick(),
+          Brick(),
+          Brick(),
+        ],
       ),
+    );
+  }
+}
+
+class Brick extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: 40.0,
+      height: 10.0,
+      color: Colors.green,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0), color: Colors.green),
     );
   }
 }
